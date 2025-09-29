a3f74117ef4ee7138421fe1c1ff627169230c519
Sergey <s.pozharitsky@intelico.su>
2025-09-29 12:03:22 +0300

    completed lesson-02/01.js

--- lesson-02/01.js
+++ lesson-02/01.js
@@ -13,6 +13,6 @@ const isVerifiedUser = true
 const hasSpecialPermission = true
 const hasTemporaryPass = false
 
-let isAccess
+let isAccess = ((isAdmin || isVerifiedUser) && (hasSpecialPermission || hasTemporaryPass));
 
 // your code
