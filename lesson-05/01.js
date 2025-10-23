3c6f18720d47f8628d6ffac79ff041a1a362564a
Sergey <s.pozharitsky@intelico.su>
2025-10-23 09:35:33 +0300

    completed lesson-05/01.js

--- lesson-05/01.js
+++ lesson-05/01.js
@@ -15,5 +15,11 @@ const game = {
     gold: 250,
     lumber: 100,
   },
-  addResource() {}
+  addResource(resource, amount) {
+    if (!(resource in this.resources)) {
+      console.log("Invalid resource");
+      return;
+    }
+    this.resources.resource += amount
+  }
 }
